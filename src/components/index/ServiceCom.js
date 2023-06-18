777f2417c03eefbc9c4398ba52d672d6257c5582
Valkon <96625039+Nawija@users.noreply.github.com>
2023-06-18 11:24:23 +0200

    ddd

--- src/components/index/ServiceCom.js
+++ src/components/index/ServiceCom.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useStaticQuery, graphql, Link } from "gatsby";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { GatsbyImage, StaticImage, getImage } from "gatsby-plugin-image";
 
 const ServiceCom = () => {
     const data = useStaticQuery(
@@ -37,17 +37,28 @@ const ServiceCom = () => {
                     <Link
                         to={node.slug}
                         key={node.id}
-                        className="px-3 py-6 border-2 border-gray-400 md:hover:bg-white md:hover:border-blue-400 group md:transition rounded-md md:hover:scale-[1.01] md:hover:shadow-xl"
+                        className="relative px-3 py-6 my-4 border-2 border-transparent shadow-lg shadow-yellow-500 bg-gradient-to-tr from-amber-500 to-amber-300 md:hover:bg-white md:hover:border-yellow-500 group md:transition rounded-md md:hover:scale-[1.01] md:hover:shadow-xl md:hover:shadow-amber-500"
                     >
                         <GatsbyImage
                             image={getImage(node.img)}
                             alt={node.img.alt}
                             title={node.img.title}
                         />
-                        <h2 className="title-sm group-hover:text-blue-500 md:transition-colors">
+                        <h2 className="title-sm">
                             {node.title}
                         </h2>
                         <p>{node.desc}</p>
+                        <StaticImage
+                            style={{
+                                position: "absolute",
+                                bottom: "-24px",
+                                left: "50%",
+                                transform: "translateX(-50%)"
+                            }}
+                            placeholder="none"
+                            height={40}
+                            src="../../assets/premium.png"
+                        />
                     </Link>
                 ))}
             </div>
